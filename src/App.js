5371c6d8391d4def342fc2e3af7076f20a6f6f43
Rafael Sena <rafaelsenna13@gmail.com>
2020-02-22 14:41:28 -0300

    changed color pallete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header/Header';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import deepPurple from "@material-ui/core/colors/deepPurple";
+import pink from "@material-ui/core/colors/pink";
 
 export default function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: { main: "#7e57c2" },
+      secondary: { main: "#ff4081" }
+    }
+  });
+
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Header />
-    </div>
+    </ThemeProvider>
   );
 }
