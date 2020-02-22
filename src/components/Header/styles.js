1b286efb267da53ff9aaa9110e1e1b6bd09d7be9
Rafael Sena <rafaelsenna13@gmail.com>
2020-02-22 14:41:56 -0300

    moved css to a styles file

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,55 +3,58 @@ import { fade, makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    marginBottom: "2rem"
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    display: 'block',
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
   },
   title: {
-    outline: "none",
     flexGrow: 1,
-    justifySelf: "start"
-  },
-  logo: {
-    textDecoration: "none",
-    fontWeight: "bold",
-    color: "#fff"
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
   },
   search: {
-    position: "relative",
+    position: 'relative',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
       marginLeft: theme.spacing(1),
-      width: "auto"
-    }
+      width: 'auto',
+    },
   },
   searchIcon: {
     width: theme.spacing(7),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   inputRoot: {
-    color: theme.palette.common.white
+    color: 'inherit',
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
       width: 120,
-      "&:focus": {
-        width: 200
-      }
-    }
-  }
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
 }));
 
 export default useStyles;
