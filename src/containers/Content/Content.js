0f5254ed0d236fe7a4ffecdb7c62420bbbca36b2
Rafael Sena <rafaelsenna13@gmail.com>
2020-02-22 23:44:53 -0300

    added Todo delete button

--- src/containers/Content/Content.js
+++ src/containers/Content/Content.js
@@ -7,16 +7,22 @@ import InputBase from "@material-ui/core/InputBase";
 
 const Content = props => {
   const classes = useStyles();
-  const [todos, setTodos] = useState([{text: 'Jogar LOL'}]);
+  const [todos, setTodos] = useState([{ text: "Jogar LOL" }]);
 
-  const handleKeyPress = (event) => {
-    if(event.key === "Enter") {
-      const todo = {text: event.target.value}
+  const handleKeyPress = event => {
+    if (event.key === "Enter") {
+      const todo = { text: event.target.value };
       const newState = [...todos, todo];
       setTodos(newState);
       event.target.value = "";
     }
-  }
+  };
+
+  const deleteTodo = index => {
+    const newTodos = todos.slice();
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
   let todoList = (
     <h4 className={classes.empty}>
@@ -26,7 +32,7 @@ const Content = props => {
 
   if (todos.length) {
     todoList = todos.map((todo, index) => (
-      <Todo key={index} content={todo} index={index} />
+      <Todo key={index} text={todo.text} index={index} deleteTodo={deleteTodo} />
     ));
   }
 
@@ -37,8 +43,11 @@ const Content = props => {
         <div className={classes.addTodo}>
           <InputBase
             placeholder="Escreva sua tarefa e pressione Enter"
-            classes={{input: classes.inputInput, root: classes.inputRoot}}
-            inputProps={{ "aria-label": "add-todo", "onKeyPress": event => handleKeyPress(event) }}
+            classes={{ input: classes.inputInput, root: classes.inputRoot }}
+            inputProps={{
+              "aria-label": "add-todo",
+              onKeyPress: event => handleKeyPress(event)
+            }}
           />
         </div>
         <List>{todoList}</List>
